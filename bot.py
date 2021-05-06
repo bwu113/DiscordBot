c043ecc0f1f93e55a5e528329d92b236e6e19e1c
bwu113 <bowenwu112@gmail.com>
2021-05-06 16:57:20 -0400

    fixed crash

--- bot.py
+++ bot.py
@@ -5,12 +5,6 @@ from discord.ext import commands
 #Character used before a command
 entryBot = commands.Bot(command_prefix='.')
 
-def read_token():
-    with open('token.txt', 'r') as f:
-        lines = f.readlines()
-        return lines[0].strip()
-token = read_token()
-
 @entryBot.command()
 async def load(ctx, extension):
     entryBot.load_extension(f'cogs.{extension}')
