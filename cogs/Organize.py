2ab16449f73df056d8a45d1f27a4e386803538c4
bwu113 <bowenwu112@gmail.com>
2021-05-06 18:39:29 -0400

    Fixing delete channel

--- cogs/Organize.py
+++ cogs/Organize.py
@@ -10,9 +10,9 @@ class Organize(commands.Cog):
         await ctx.guild.create_text_channel(channel_name)
         await ctx.send(f'A new text channel called {channel_name} was created.')
 
-    async def removechannel(self, ctx, channel: discord.channel_name):
-        await channel.delete()
-        await ctx.send(f'Channel {channel} was removed.')
+    async def removechannel(self, ctx, channel_name):
+        await ctx.TextChannel.delete(channel_name, reason="Deleted by bot")
+        await ctx.send(f'Channel {channel_name} was removed.')
 
 def setup(client):
     client.add_cog(Organize(client))
